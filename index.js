24322cffcbb1795c4ea1ee604319a91c648da1fd
Wellerson <wllrsn.c@gmail.com>
2023-07-30 20:17:47 -0300

    update version

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // Importar as dependências
 const express = require("express");
+require('dotenv').config()
 
 // Criar uma instância do Express
 const app = express();
-const port = 80; // Porta em que o servidor irá rodar
+const port = process.env.PORT || 80; // Porta em que o servidor irá rodar
 
 // Definir uma rota básica
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "server is running" });
+  res.status(200).json({ message: `server is running ${process.env.HOST}` });
 });
 
 // Definir outra rota
 app.get("/about", (req, res) => {
-  res.status(200).json({ message: "Page about express." });
+  res.status(200).json({ message: `Page about express. ${process.env.TEST}` });
 });
 
 app.get("/user/:id", (req, res) => {
