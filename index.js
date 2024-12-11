ffcc96c78ea60af3d43e54f040371bab8f6026d3
Wellerson <wllrsn.c@gmail.com>
2024-12-10 23:27:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.get("/about", (req, res) => {
   res.status(200).json({ message: `Page about express. ${process.env.TEST}` });
 });
 
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: `Page test express. ${process.env.TEST}` });
+});
+
 app.get("/user/:id", (req, res) => {
   const userId = req.params.id;
   res.status(200).json({ message: `user profile with ID ${userId}` });
